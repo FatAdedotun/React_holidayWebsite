14a3934608729f1aa53d26dd2b8434bbbf51e1c1
Adedotun Fatokun <fatokun.adedotun@gmail.com>
2021-06-16 01:01:34 +0100

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../App.css'
+import Cards from '../Card/Cards';
+import HeroSection from '../Hero/HeroSection';
+
+function Home () {
+    return (
+        <>
+            <HeroSection />
+            <Cards />
+        </>
+    )
+}
+
+export default Home
